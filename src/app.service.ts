be2bfeabcb3346f3291778b76905735cf2d40487
77-ahmad-collab <ahmaq098@gmail.com>
2022-03-07 16:11:22 +0500

    deployment setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -14,7 +14,6 @@ import { Migration, MigrationDocument } from './schemas/migration.schema';
 import getSignatures from './utils/getSignatures';
 
 const Web3 = require('web3');
-
 @Injectable()
 export class AppService {
   constructor(
